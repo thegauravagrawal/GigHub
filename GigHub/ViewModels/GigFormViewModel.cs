6c683ed015b6641beb7b56d5c4f1f8d1b27f6720
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-01-07 11:38:31 +0530

    Implement Saving a Gig.

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            }
+        }
+    }
+}
